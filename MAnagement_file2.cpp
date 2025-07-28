a12fc72c3b4a9fd1e90678f07dca5f1edb8d28ab
XKIrito <calloracj90@gmail.com>
2025-07-28 23:41:05 +0800

    Add files via upload

---
+++ MAnagement_file2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <direct.h>
+#include <filesystem>
+#include <string>
+using namespace std;
+
+
+  void MainMenu(){
+  	
+  	cout<<"[1]List Files"<<endl;
+  	cout<<"[2]Create Directory"<<endl;
+  	cout<<"[3]Change Directory"<<endl;
+	cout<<"[4]Exit Files"<<endl;
+  	
+	  }
+	  
+	  
+	  void ListFiles(){
+  	
+  	cout<<"[1]List All Files  "<<endl;
+  	cout<<"[2]List Files by Extension "<<endl;
+  	cout<<"[3]List Files by Pattern "<<endl;
+	
+  	
+	  }
+	  
+	  
+	  
+	 void CreateDirectory() {
+	 
+	 
+	 
+	 }
+	 
+	 
+	 void ChangeDirectory(){
+	 	
+	 	
+	 	
+	 	
+	 }
+	 
+  	
+
+  
+ 
+
+
+
+
+	int main(){
+	
+		MainMenu();	
+	
+			int choice;
+	
+				cin>>choice;	
+		
+	switch(choice){
+		case 1:
+			
+			
+			break;
+		case 2:
+			
+			
+			break;
+		case 3:
+			
+			
+			break;
+		case 4:
+		
+			
+			break;
+			
+		
+		
+		
+		
+	}	
+		
+		
+		
+		
+		
+	}
