c4cc048e2526ed8678175c27319a19b7aa23abce
XKIrito <calloracj90@gmail.com>
2025-07-29 20:33:50 +0800

    Update MAnagement_file2.cpp

--- MAnagement_file2.cpp
+++ MAnagement_file2.cpp
@@ -1,87 +1,17 @@
 #include <iostream>
-#include <direct.h>
-#include <filesystem>
 #include <string>
 using namespace std;
 
+void mainMenu() {
+    cout << "\nDirectory Management System\n";
+    cout << "[1] List Files\n";
+    cout << "[2] Create Directory\n";
+    cout << "[3] Change Directory\n";
+    cout << "[4] Exit\n";
+    cout << "Enter your choice: ";
+}
 
-  void MainMenu(){
-  	
-  	cout<<"[1]List Files"<<endl;
-  	cout<<"[2]Create Directory"<<endl;
-  	cout<<"[3]Change Directory"<<endl;
-	cout<<"[4]Exit Files"<<endl;
-  	
-	  }
-	  
-	  
-	  void ListFiles(){
-  	
-  	cout<<"[1]List All Files  "<<endl;
-  	cout<<"[2]List Files by Extension "<<endl;
-  	cout<<"[3]List Files by Pattern "<<endl;
-	
-  	
-	  }
-	  
-	  
-	  
-	 void CreateDirectory() {
-	 
-	 
-	 
-	 }
-	 
-	 
-	 void ChangeDirectory(){
-	 	
-	 	
-	 	
-	 	
-	 }
-	 
-  	
-
-  
- 
-
-
-
-
-	int main(){
-	
-		MainMenu();	
-	
-			int choice;
-	
-				cin>>choice;	
-		
-	switch(choice){
-		case 1:
-			
-			
-			break;
-		case 2:
-			
-			
-			break;
-		case 3:
-			
-			
-			break;
-		case 4:
-		
-			
-			break;
-			
-		
-		
-		
-		
-	}	
-		
-		
-		
-		
-		
-	}
+int main() {
+    mainMenu();
+    return 0;
+}
